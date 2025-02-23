e46554bd31c0bd785f6fc3544cd2cf386e1d4dc9
maqss <maksymgulko@gmail.com>
2025-02-23 12:53:28 +0100

    add build script, prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 import mongoConnect from "./db/mongoConnect.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
 const app = express();
 
@@ -19,6 +20,8 @@ cloudinary.config({
 });
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 
@@ -34,7 +37,15 @@ app.use("/api/posts", postRoutes);
 
 app.use("/api/notifications", notificationRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-  console.log(`runnung on PORT: ${PORT}`);
+  console.log(`running on PORT: ${PORT}`);
   mongoConnect();
 });
